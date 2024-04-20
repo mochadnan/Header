9d98865f6a8f713fc0b8fe2d10e81f5f3dc38193
mochadnan <rinahrey@gmail.com>
2024-04-20 23:44:32 +0700

    Update Header.js

--- Header.js
+++ Header.js
@@ -5,10 +5,10 @@ document.querySelector(".writeHeader").innerHTML = `
         <a href="index.html"><img src="https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEjF3Vv5yFH5DVD_j0dLxb-EEN0F3_SjPhy4q8ZYyKJS6dAjMJxqE0YOIq1QDo7YKjeKaRbmTmRB6E238CA0QNf0Y9OklY1sE81nL_bFfFNDQs3dJzLWulMYlgjBCM988rRpZAyZntB0oVnU4GIECIgn6vwv82aw7bIozF1ly4Ejo4MY0CSLZj3WNECIvXH8/s1600/logo.png" alt="Logo" /></a> 
 
         <div id="nav">
-            <a href="index.html">HOME</a> |
-            <a href="archive.html">ARCHIVE</a> |
-            <a href="about.html">ABOUT</a> |
-            <a href="characters.html">CHARACTERS</a>
+            <a href="https://singgung79.blogspot.com/2024/04/index..html">HOME</a> |
+            <a href="https://singgung79.blogspot.com/p/webcomic-archive-archive-indexpage-index.html">ARCHIVE</a> |
+            <a href="https://singgung79.blogspot.com/p/ruth-and-dianne-are-starting-band-about.html">ABOUT</a> |
+            <a href="https://singgung79.blogspot.com/p/webcomic-characters-characters-all-of.html">CHARACTERS</a>
         </div>
     </header>
 `;
